cc3fec942b98fbdb92181016383ab8856e752964
Yago-P <yago.exe@gmail.com>
2026-07-20 14:40:27 -0300

    feat: create reusable Button component with typed props

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import './Button.css'
+
+type ButtonProps = {
+    texto: string
+}
+
+function Button({texto} : ButtonProps){
+
+    return(
+        <button>
+            {texto}
+        </button>
+    )
+
+}
+
+export default Button
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,38 +1,46 @@
 import taskegerLogo from '../../assets/Subaru Brazil 2026.jpeg' 
 import './Header.css'
+import Button from '../Button/Button'
 
 function Header(){
     
     return(
-        <header className='site-header'>
-            <div className='header-content'>
-                <a href="/home" className='brand'>
-                    <img src={taskegerLogo} alt="Logo Provisório do Taskeger" width="60" height="60" className='brand-icon'/>
-                    <span className='brand-title'>
-                    Taskeger
-                    </span>
-                
-                </a>
-                
-                <div className='header-right'>
-                    <div className='auth-actions'>
-                        <a href="/login" className='login-button'>
-                        Login
-                        </a>
+        <>
+            <header className='site-header'>
+                <div className='header-content'>
+                    <a href="/home" className='brand'>
+                        <img src={taskegerLogo} alt="Logo Provisório do Taskeger" width="60" height="60" className='brand-icon'/>
+                        <span className='brand-title'>
+                        Taskeger
+                        </span>
+                    
+                    </a>
+                    
+                    <div className='header-right'>
+                        <div className='auth-actions'>
+                            <a href="/login" className='login-button'>
+                            Login
+                            </a>
 
-                        <a href="/sign-in" className='sign-in-button'>
-                        Sign In
-                        </a>
+                            <a href="/sign-in" className='sign-in-button'>
+                            Sign In
+                            </a>
+                        </div>
+                    
+                        <button className='menu-button'>
+                            ☰
+                        </button>
                     </div>
-                
-                    <button className='menu-button'>
-                        ☰
-                    </button>
+
                 </div>
 
-            </div>
+                <div>
+                    <Button texto = 'Login' />
+                    
+                </div>    
 
-        </header>
+            </header>
+        </>
     )
 }
 
