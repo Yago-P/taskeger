aa21bfb3392aa88fae9aa6730bf1bef76e63c1f9
Yago-P <yago.exe@gmail.com>
2026-07-28 09:26:00 -0300

    refactor: add shared layout with react router outlet

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,19 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import HomePage from './pages/Home/HomePage.tsx'
 import LoginPage from './pages/Login/LoginPage.tsx'
 import SignUpPage from './pages/SignUp/SignUpPage.tsx'
+import MainLayout from './layouts/MainLayout/MainLayout.tsx'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomePage />}/>
-        <Route path='login' element={<LoginPage />}/>
-        <Route path='sign-up' element={<SignUpPage />} />
-        
-        
+        <Route element={<MainLayout />} >
+          <Route path='/' element={<HomePage />}/>
+          <Route path='login' element={<LoginPage />}/>
+          <Route path='sign-up' element={<SignUpPage />} />
 
+        </Route>
       </Routes>
 
 
